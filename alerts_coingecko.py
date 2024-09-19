38e63e53cd89f7e88422287a925d477dd2f7a793
Jstvz <jcestevez739@gmail.com>
2024-09-19 16:22:48 -0500

    Crea la función para evaluar los datos del JSON

--- alerts_coingecko.py
+++ alerts_coingecko.py
@@ -13,7 +13,7 @@ def get_prices():
     try:
         getdata = requests.get(url, params=PARAMS)
         if getdata.status_code == 200:
-            data = getdata.json()
+            data = getdata.json() 
             print(data)
             return data
         else:
@@ -23,5 +23,30 @@ def get_prices():
             print("Error durante la solicitud a la API:", e)
             return None
 
+def value_prices():
+    # Se obtiene el JSON con los datos 
+    prices = get_prices()
+    
+    # Se continua con el proceso si la funcion retorna datos validos
+    if prices:
+
+        alert_msg = ""
+
+        bitcoin_price  = prices.get('bitcoin', {}).get('usd')
+        ethereum_price = prices.get('ethereum', {}).get('usd')
+        binancecoin_price = prices.get('binancecoin', {}).get('usd')
+        
+        # Validación del los datos
+        if bitcoin_price and bitcoin_price > 63000:
+            alert_msg += f" Bitcoin ha superado los $30,000 USD, su precio actual es: ${bitcoin_price}\n"
+
+        if ethereum_price and ethereum_price > 2000:
+            alert_msg += f" Ethereum ha superado los $2,0000 USD, su precio actual es: ${ethereum_price}\n"
+
+        if binancecoin_price and binancecoin_price > 500:
+            alert_msg += f" Binancecoin ha superado los $3000 USD, su precio actual es: ${binancecoin_price}\n"
+    
+    print(alert_msg)
+
 if __name__ == "__main__":
-    get_prices()
+    value_prices()
